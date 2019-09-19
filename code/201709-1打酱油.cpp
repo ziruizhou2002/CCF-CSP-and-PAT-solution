0d58179313d0f2726f8c418a09cfc54d6147fc28
日沉云起 <csjiangfeng@gmail.com>
2019-09-19 21:00:02 +0800

    Update 201709-1打酱油.cpp

--- code/201709-1打酱油.cpp
+++ code/201709-1打酱油.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int N;
-	scanf("%d",&N);
-	printf("%d",N/10+N/50*2+N%50/30);
-} 
+int main() {
+    int n;
+    cin >> n;
+    cout << n / 10 + n / 50 * 2 + n % 50 / 30;
+}
