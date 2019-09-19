71daa7b84c7e0ac7fb52903fede693d40a6bc246
日沉云起 <csjiangfeng@gmail.com>
2019-09-19 19:46:35 +0800

    Update 201812-1小明上学.cpp

--- code/201812-1小明上学.cpp
+++ code/201812-1小明上学.cpp
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int red,yellow,green,N,a,b,ans=0;
-    scanf("%d%d%d%d",&red,&yellow,&green,&N);
-    while(~scanf("%d%d",&a,&b))
-        if(a==0||a==1)
-            ans+=b;
-        else if(a==2)
-            ans+=b+red;
-    printf("%d",ans);
+int main() {
+    int red, yellow, green, n, k, t, ans = 0;
+    cin >> red >> yellow >> green >> n;
+    while (cin >> k >> t)
+        if (k == 0 || k == 1)
+            ans += t;
+        else if (k == 2)
+            ans += t + red;
+    cout << ans;
     return 0;
 }
