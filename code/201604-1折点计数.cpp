f6162172dc91e0f8ca678d2b0093b6ea6b439839
richenyunqi <csjiangfeng@gmail.com>
2019-10-10 14:38:04 +0800

    2019.10.10

--- code/201604-1折点计数.cpp
+++ code/201604-1折点计数.cpp
@@ -1,15 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int N;
-	scanf("%d",&N);
-	int a[N];
-	for(int i=0;i<N;++i)
-		scanf("%d",&a[i]);
-	int num=0;
-	for(int i=1;i<N-1;++i)
-		if((a[i-1]<a[i]&&a[i]>a[i+1])||(a[i-1]>a[i]&&a[i]<a[i+1]))
-			++num;
-	printf("%d",num);
-	return 0;
+int main() {
+    int n, ans = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    for (int i = 1; i < n - 1; ++i)
+        if ((a[i - 1] < a[i] && a[i] > a[i + 1]) || (a[i - 1] > a[i] && a[i] < a[i + 1]))
+            ++ans;
+    cout << ans;
+    return 0;
 }
