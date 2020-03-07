a10189e37ce8a4a27447e98a1c6eac5b919eca7c
richenyunqi <csjiangfeng@gmail.com>
2020-03-07 20:56:40 +0800

    成绩排名

--- PAT/Basic Level/1004. 成绩排名.cpp
+++ PAT/Basic Level/1004. 成绩排名.cpp
@@ -1,21 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Student{
-    string name="",number="";
-    int score;
-    Student(int s=0):score(s){}
+using gg = long long;
+struct Student {
+    string name, number;
+    gg score;
+    Student(gg s = 0) : score(s) {}
 };
-int main(){
-    int N;
-    scanf("%d",&N);
-    Student Max(-1),Min(101),temp;
-    for(int i=0;i<N;++i){
-        cin>>temp.name>>temp.number>>temp.score;
-        if(Max.score<temp.score)
-            Max=temp;
-        if(Min.score>temp.score)
-            Min=temp;
+int main() {
+    gg n;
+    cin >> n;
+    Student Max(-1), Min(101), temp;
+    for (gg i = 0; i < n; ++i) {
+        cin >> temp.name >> temp.number >> temp.score;
+        if (Max.score < temp.score)
+            Max = temp;
+        if (Min.score > temp.score)
+            Min = temp;
     }
-    printf("%s %s\n%s %s",Max.name.c_str(),Max.number.c_str(),Min.name.c_str(),Min.number.c_str());
+    cout << Max.name << " " << Max.number << "\n" << Min.name << " " << Min.number;
     return 0;
 }
