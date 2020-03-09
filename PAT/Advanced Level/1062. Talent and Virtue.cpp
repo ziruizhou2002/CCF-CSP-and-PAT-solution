a13de1797f2cff48b86a1c12e62698f827f91b47
richenyunqi <csjiangfeng@gmail.com>
2020-03-09 15:48:52 +0800

    德才论

--- PAT/Advanced Level/1062. Talent and Virtue.cpp
+++ PAT/Advanced Level/1062. Talent and Virtue.cpp
@@ -1,42 +1,35 @@
-#include<bits/stdc++.h>  
-using namespace std;  
-struct Student{  
-    int id,de,cai,sum;//学生id、德分、才分、总分
-    int leibie;//定义类别信息  
-};  
-bool cmp(const Student&s1,const Student&s2){  //比较函数 
-    if(s1.leibie!=s2.leibie)  
-        return s1.leibie<s2.leibie;  
-    else if(s1.sum!=s2.sum)  
-        return s1.sum>s2.sum;  
-    else if(s1.de!=s2.de)  
-        return s1.de>s2.de;  
-    else  
-        return s1.id<s2.id;  
-}  
-int main(){
-    int N,L,H,size=0;
-    scanf("%d%d%d",&N,&L,&H);  
-    Student student[N];  
-    for(int i=0;i<N;++i){  
-        scanf("%d%d%d",&student[size].id,&student[size].de,&student[size].cai);  
-        if(student[size].de>=L&&student[size].cai>=L){  
-            student[size].sum=student[size].de+student[size].cai;   
-            if(student[size].de>=H&&student[size].cai>=H)//分类 
-                student[size].leibie=1;  
-            else if(student[size].de>=H&&student[size].cai<H)  
-                student[size].leibie=2;  
-            else if(student[size].de<H&&student[size].cai<H  
-                    &&student[size].de>=student[size].cai)  
-                student[size].leibie=3;  
-            else  
-                student[size].leibie=4;  
-            ++size;  
-        }  
-    }  
-    sort(student,student+size,cmp);//排序
-    printf("%d\n",size);  
-    for(int i=0;i<size;++i)
-        printf("%d %d %d\n",student[i].id,student[i].de,student[i].cai);  
-    return 0;  
-}  
+#include <bits/stdc++.h>
+using namespace std;
+using gg = long long;
+struct Student {
+    gg id, de, cai, total, level;
+    Student(gg i, gg d, gg c, gg le) : id(i), de(d), cai(c), level(le) { total = de + cai; }
+};
+int main() {
+    vector<Student> students;
+    gg n, l, h, id, de, cai, level;
+    cin >> n >> l >> h;
+    while (n--) {
+        cin >> id >> de >> cai;
+        if (de < l or cai < l)
+            continue;
+        if (de >= h and cai >= h) {
+            level = 1;
+        } else if (de >= h and cai < h) {
+            level = 2;
+        } else if (de < h and cai < h and de >= cai) {
+            level = 3;
+        } else {
+            level = 4;
+        }
+        students.push_back(Student(id, de, cai, level));
+    }
+    sort(students.begin(), students.end(), [](const Student& s1, const Student& s2) {
+        return tie(s1.level, s2.total, s2.de, s1.id) < tie(s2.level, s1.total, s1.de, s2.id);
+    });
+    cout << students.size() << '\n';
+    for (auto& s : students) {
+        printf("%08lld %lld %lld\n", s.id, s.de, s.cai);
+    }
+    return 0;
+}
