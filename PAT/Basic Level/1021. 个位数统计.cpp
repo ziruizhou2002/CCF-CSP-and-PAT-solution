3947c3dcfec51dae134924a718ca7be6e3f98dca
richenyunqi <csjiangfeng@gmail.com>
2020-03-09 18:43:55 +0800

    update

--- PAT/Basic Level/1021. 个位数统计.cpp
+++ PAT/Basic Level/1021. 个位数统计.cpp
@@ -1,13 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string input ;
-    getline(cin,input);
-    map<char,int>result;
-    for(int i=0;input[i]!='\0';++i)
-        ++result[input[i]];
-    for(auto i=result.cbegin();i!=result.cend();++i){
-        printf("%c:%d\n",i->first,i->second);
+using gg = long long;
+int main() {
+    array<gg, 10> h{};
+    string s;
+    cin >> s;
+    for (char c : s) {
+        ++h[c - '0'];
+    }
+    for (int i = 0; i < h.size(); ++i) {
+        if (h[i] > 0) {
+            cout << i << ':' << h[i] << '\n';
+        }
     }
     return 0;
 }
