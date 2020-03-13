e86ddec02d2c8472986f7bc6a80eceb62d42b287
richenyunqi <csjiangfeng@gmail.com>
2020-03-13 18:37:56 +0800

    update

--- PAT/Basic Level/1051. 复数乘法.cpp
+++ PAT/Basic Level/1051. 复数乘法.cpp
@@ -1,19 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    double r1,p1,r2,p2;
-    scanf("%lf%lf%lf%lf",&r1,&p1,&r2,&p2);
-    double a=r1*cos(p1)*r2*cos(p2)-r1*sin(p1)*r2*sin(p2);
-    double b=r1*sin(p1)*r2*cos(p2)+r1*cos(p1)*r2*sin(p2);
-    if (a + 0.005 >= 0 && a < 0)
-        printf("0.00");
-    else
-        printf("%.2f", a);
-    if(b >= 0)
-        printf("+%.2fi", b);
-    else if (b + 0.005 >= 0 && b < 0)
-        printf("+0.00i");
-    else
-        printf("%.2fi", b);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    double r1, r2, p1, p2;
+    cin >> r1 >> p1 >> r2 >> p2;
+    double a = r1 * cos(p1) * r2 * cos(p2) - r1 * sin(p1) * r2 * sin(p2);
+    double b = r1 * sin(p1) * r2 * cos(p2) + r1 * cos(p1) * r2 * sin(p2);
+    a = a < 0 and a > -0.005 ? 0 : a;
+    b = b < 0 and b > -0.005 ? 0 : b;
+    cout << fixed << setprecision(2) << a << showpos << b << 'i';
     return 0;
 }
