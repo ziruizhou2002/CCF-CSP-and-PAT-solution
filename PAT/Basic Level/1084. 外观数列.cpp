36898e2dcb99edd9c09ef29d60787a281c67a258
richenyunqi <csjiangfeng@gmail.com>
2020-03-15 18:00:12 +0800

    update

--- PAT/Basic Level/1084. 外观数列.cpp
+++ PAT/Basic Level/1084. 外观数列.cpp
@@ -1,17 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string str;
-    int N,k;
-    cin>>str>>N;
-    for(int i=1;i<N;++i){
-        string s="";//临时储存结果的字符串
-        for(int j=0;j<str.size();j=k){
-            for(k=j;k<str.size()&&str[k]==str[j];++k);//找到与str[j]字符不同的字符的位置
-            s+=str[j]+to_string(k-j);//在s末尾加上str[j]字符和连续str[j]字符的个数
+using gg = long long;
+int main() {
+    string s;
+    gg n;
+    cin >> s >> n;
+    while (--n) {
+        string t;
+        for (gg i = 0, j; i < s.size(); i = j) {
+            j = s.find_first_not_of(s[i], i + 1);
+            if (j == -1)
+                j = s.size();
+            t += string(1, s[i]) + to_string(j - i);
         }
-        str=s;
+        s = t;
     }
-    cout<<str;
+    cout << s;
     return 0;
 }
