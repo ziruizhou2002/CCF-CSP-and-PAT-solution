4bc6db25989aeb5b8ed0c1daa47f3a774354d67a
richenyunqi <csjiangfeng@gmail.com>
2020-03-29 11:08:37 +0800

    update

--- PAT/Advanced Level/1143. Lowest Common Ancestor.cpp
+++ PAT/Advanced Level/1143. Lowest Common Ancestor.cpp
@@ -1,62 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Node{//结点类
-    int data,father,level;//权值、父节点在pre中的下标、深度
-    Node(int d=0,int f=-1,int l=0):data(d),father(f),level(l){}
-};
-const int MAX=10005;
-Node pre[MAX];
-int n,m,in[MAX];
-void createTree(int root,int left,int right,int father,int level){//确定每个结点的父节点和深度
-    if(left>right)
-        return;
-    int i=left;
-    while(i<=right&&in[i]!=pre[root].data)
-        ++i;
-    pre[root]=Node(pre[root].data,father,level);//确定当前子树根结点的父节点和深度
-    createTree(root+1,left,i-1,root,level+1);//递归处理左子树
-    createTree(root+1+i-left,i+1,right,root,level+1);//递归处理右子树
-}
-int main(){
-    scanf("%d%d",&m,&n);
-    for(int i=0;i<n;++i){
-        scanf("%d",&pre[i].data);
-        in[i]=pre[i].data;
-    }
-    sort(in,in+n);//中序遍历序列
-    createTree(0,0,n-1,-1,0);
-    for(int i=0;i<m;++i){
-        int a,b;
-        scanf("%d%d",&a,&b);
-        int ia=n,ib=n;
-        for(int i=0;i<n;++i){//找到两个结点在pre数组中的下标
-            if(pre[i].data==a)
-                ia=i;
-            if(pre[i].data==b)
-                ib=i;
-        }
-        if(ia==n&&ib==n){
-            printf("ERROR: %d and %d are not found.\n",a,b);
-        }else if(ia==n){
-            printf("ERROR: %d is not found.\n",a);
-        }else if(ib==n){
-            printf("ERROR: %d is not found.\n",b);
-        }else{
-            bool f=true;//true表示a的深度更大
-            if(pre[ia].level<pre[ib].level){//让ia指向深度更大的结点
-                swap(ia,ib);
-                f=false;
-            }
-            while(pre[ia].level>pre[ib].level)//将二者调整到同一深度
-                ia=pre[ia].father;
-            if(ia==ib){
-                printf("%d is an ancestor of %d.\n",pre[ia].data,f?a:b);
-            }else{
-                while(ia!=ib){//ia,ib同时向上调整，直至二者指向同一结点
-                    ia=pre[ia].father;
-                    ib=pre[ib].father;
-                }
-                printf("LCA of %d and %d is %d.\n",a,b,pre[ia].data);
+using gg = long long;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    gg mi, ni, u, v;
+    cin >> mi >> ni;
+    vector<gg> pre(ni);
+    for (gg i = 0; i < ni; ++i)
+        cin >> pre[i];
+    unordered_set<gg> us(pre.begin(), pre.end());
+    while (mi--) {
+        cin >> u >> v;
+        bool f1 = us.count(u), f2 = us.count(v);
+        if (not f1 and not f2) {
+            cout << "ERROR: " << u << " and " << v << " are not found.\n";
+        } else if (not f1 or not f2) {
+            cout << "ERROR: " << (not f1 ? u : v) << " is not found.\n";
+        } else {
+            gg ans = *find_if(pre.begin(), pre.end(),
+                              [u, v](gg a) { return (a - u) * (a - v) <= 0; });
+            if (ans == u or ans == v) {
+                cout << (ans == u ? u : v) << " is an ancestor of "
+                     << (ans == u ? v : u) << ".\n";
+            } else {
+                cout << "LCA of " << u << " and " << v << " is " << ans << ".\n";
             }
         }
     }
--- PAT/Advanced Level/1151. LCA in a Binary Tree.cpp
+++ PAT/Advanced Level/1151. LCA in a Binary Tree.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 using gg = long long;
-const gg no = INT_MAX + 1ll;  //表示空结点
 unordered_map<gg, gg> um;  //存储结点与其在中根序列中的位置
-gg LCA(vector<gg>& pre, vector<gg>& in, gg r, gg left, gg right, gg p, gg q) {
-    if (left > right)  //序列为空，返回空指针
-        return no;
-    if (pre[r] == p or pre[r] == q)
-        return pre[r];
-    gg i = um[pre[r]];
-    auto k1 = LCA(pre, in, r + 1, left, i - 1, p, q),
-         k2 = LCA(pre, in, r + 1 + i - left, i + 1, right, p, q);
-    return k1 == no ? k2 : k2 == no ? k1 : pre[r];
+gg LCA(vector<gg>& pre, gg r, gg left, gg right, gg p, gg q) {
+    gg i = um[pre[r]], pi = um[p], qi = um[q];  //根结点、p、q在中根序列中的位置
+    return (i - pi) * (i - qi) <= 0 ?
+               pre[r] :  //找到了p,q或者p、q分居在根结点两棵子树，返回根结点
+               // p、q都在左子树，向左子树递归遍历
+               pi < i ? LCA(pre, r + 1, left, i - 1, p, q) :
+                        // p、q都在右子树，向右子树递归遍历
+                   LCA(pre, r + 1 + i - left, i + 1, right, p, q);
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     gg mi, ni, u, v;
     cin >> mi >> ni;
-    vector<gg> pre(ni), in(ni);
+    vector<gg> pre(ni);
     for (gg i = 0; i < ni; ++i) {
-        cin >> in[i];
-        um[in[i]] = i;
+        cin >> u;
+        um[u] = i;
     }
     for (gg i = 0; i < ni; ++i)
         cin >> pre[i];
@@ -30,12 +28,10 @@ int main() {
         bool f1 = um.count(u), f2 = um.count(v);
         if (not f1 and not f2) {
             cout << "ERROR: " << u << " and " << v << " are not found.\n";
-        } else if (not f1) {
-            cout << "ERROR: " << u << " is not found.\n";
-        } else if (not f2) {
-            cout << "ERROR: " << v << " is not found.\n";
+        } else if (not f1 or not f2) {
+            cout << "ERROR: " << (not f1 ? u : v) << " is not found.\n";
         } else {
-            gg ans = LCA(pre, in, 0, 0, ni - 1, u, v);
+            gg ans = LCA(pre, 0, 0, ni - 1, u, v);
             if (ans == u or ans == v) {
                 cout << (ans == u ? u : v) << " is an ancestor of "
                      << (ans == u ? v : u) << ".\n";
